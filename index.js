c9ad63d4177069d52f52cc9ca33fb4886ab7c5cd
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2022-12-14 08:33:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word.length < 2) return true
+
+  let start = 0
+  let end = word.length - 1
+
+  while(start <= end){
+    if(word[start] != word[end]){
+      return false
+    }
+    start++
+    end--
+  }
+
+    return true
 }
 
 /* 
   Add your pseudocode here
+  return true for single letter words
+  define a variable start with 0 
+  define a variable end with the length of the word minus 1
+  loop through the word (if statement) with start as the index of the first letter and end as index of the last letter
+  return true if start and end letters match and false if they don't
+
 */
 
 /*
